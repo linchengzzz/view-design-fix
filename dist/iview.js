cc1018c47d915a6d935f89d18e565a4b561ab5d5
linchengzzz <1139489076@qq.com>
2021-01-28 15:23:29 +0800

    release 4.4.1

--- dist/iview.js
+++ dist/iview.js
@@ -30545,7 +30545,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '4.4.0',
+    version: '4.4.1',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
